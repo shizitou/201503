5411d69abd2c98fa5bb9f5e687f61ba90d3b2482
zhangrenyang-t510 <zhang_renyang@>
2015-05-30 11:19:45 +0800

    file

--- 9.file/5.copy.js
+++ 9.file/5.copy.js
@@ -1,13 +1,16 @@
 var fs = require('fs');
-var BUFFER_SIZE = 100000;
+var BUFFER_SIZE = 1;
 function copy(src,dest){
-    var readSoFar,fdsrc,fddest,read;
+    var readSoFar=0,fdsrc,fddest,read;
     var buff = new Buffer(BUFFER_SIZE);
     fdsrc = fs.openSync(src,'r');
     fddest = fs.openSync(dest,'w');
     do{
-        readSync
-        writeSync
+        //fd, buffer, offset, length, position
+        read  = fs.readSync(
+            fd,buffer,0,BUFFER_SIZE,readSoFar)
+        //fd, buffer, offset, length, position
+        fs.writeSync(fd,buffer,0,read);
     }while(read>0)
     fs.closeSync(fdsrc);
     fs.closeSync(fddest);
