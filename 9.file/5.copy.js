b9d578593b71291902734fed0b857e7992049613
zhangrenyang-t510 <zhang_renyang@>
2015-05-30 11:29:45 +0800

    file

--- 9.file/5.copy.js
+++ 9.file/5.copy.js
@@ -6,12 +6,28 @@ function copy(src,dest){
     fdsrc = fs.openSync(src,'r');
     fddest = fs.openSync(dest,'w');
     do{
-        //fd, buffer, offset, length, position
-        read  = fs.readSync(
-            fd,buffer,0,BUFFER_SIZE,readSoFar)
-        //fd, buffer, offset, length, position
-        fs.writeSync(fd,buffer,0,read);
+        read =fs.readSync(fdsrc,buff,0,BUFFER_SIZE,readSoFar);
+        fs.writeSync(fddest,buff,0,read);
+        readSoFar+=read;
     }while(read>0)
     fs.closeSync(fdsrc);
     fs.closeSync(fddest);
 }
+//copy('./test.txt','./test2.txt');
+
+function move(src,dest){
+    var readSoFar=0,fdsrc,fddest,read;
+    var buff = new Buffer(BUFFER_SIZE);
+    fdsrc = fs.openSync(src,'r');
+    fddest = fs.openSync(dest,'w');
+    do{
+        read =fs.readSync(fdsrc,buff,0,BUFFER_SIZE,readSoFar);
+        fs.writeSync(fddest,buff,0,read);
+        readSoFar+=read;
+    }while(read>0)
+    fs.closeSync(fdsrc);
+    fs.closeSync(fddest);
+    fs.unlinkSync(src);
+}
+
+move('./test.txt','./test2.txt');
